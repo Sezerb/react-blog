a625447c69490da4154f54b3dc57242d033d29fb
sezerb <sezerbaglan@gmail.com>
2020-03-24 01:20:22 +0300

    It is almost finished

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articles from './article-content';
+import { useParams } from 'react-router-dom';
+import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
 
-const ArticlePage = ({ match }) => {
-    const { name } = match.params;
+const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes:0, comments: [] })
+    const { name } = useParams();
+
+    useEffect(() => {
+        const fetchArticleInfo = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        };
+
+        fetchArticleInfo();
+    }, [name]);
 
     const matchingArticle = articles.find(
       article => article.name === name  
@@ -11,9 +25,15 @@ const ArticlePage = ({ match }) => {
     return !matchingArticle ? <h1>Article doesn't exist!</h1> : (
         <>
         <h1>{matchingArticle.name} article </h1>
+        <UpvotesSection 
+            upvotes={articleInfo.upvotes}
+            articleName={name}
+            setArticleInfo={setArticleInfo} />
+        <p>This article has {articleInfo.upvotes} upvotes</p>
         {matchingArticle.content.map((paragraph, index) => (
             <p key={index}>{paragraph}</p>
         ))}
+        <CommentsList comments={articleInfo.comments} articleName={name} setArticleInfo={setArticleInfo} />
         </>
     );  
 }
